91b2e733805c3f945ae4e7e36ae9f28c439bd04a
Grayson Gilmore <gilmoreg@live.com>
2017-09-24 13:22:40 -0700

    Flesh out models

--- server/models.py
+++ server/models.py
@@ -1,7 +1,108 @@
 '''
   Model definitions
 '''
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime as dt
-from server import db
-from flask import jsonify
-# from sqlalchemy.dialects.postgresql import JSON
+from flask_migrate import Migrate
+from server import app
+
+# Initialize database connection
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class BaseModel(db.Model):
+    """Base data model for all objects"""
+    __abstract__ = True
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def __repr__(self):
+        """Define a base way to print models"""
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        """
+        Define a base way to jsonify models, dealing with datetime objects
+        """
+        return {
+            column: value if not isinstance(value, dt.date)
+            else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class LegoSet(BaseModel):
+    '''
+    Model for watched Lego set
+    '''
+    # Equal to the official lego Set Number
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    url = db.Column(db.String(255))
+    added = db.Column(db.Datetime)
+    stock_levels = db.relationship('StockLevel', backref='legoset',
+                                   lazy='dynamic')
+    watches = db.relationship('Watch', backref='legoset',
+                              lazy='dynamic')
+
+    def __init__(self, lego_set):
+        self.id = lego_set['id']
+        self.title = lego_set['title']
+        self.image = lego_set['image']
+        self.url = lego_set['url']
+        self.added = dt.utcnow()
+
+
+class Watch(BaseModel):
+    '''
+    Stores mapping from User to LegoSet
+    Indicating this user watches this set
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    lego_set = db.Column(db.Integer, db.ForeignKey('legoset.id'))
+    added = db.Column(db.Datetime)
+
+    def __init__(self, user, lego_set):
+        self.user = user
+        self.lego_set = lego_set
+        self.added = dt.utcnow()
+
+
+class User(BaseModel):
+    '''
+    Model for User
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    added = db.Column(db.Datetime)
+    watches = db.relationship('Watch', backref='user', lazy='dynamic')
+
+    def __init__(self, email):
+        self.email = email
+        self.added = dt.utcnow()
+
+
+class StockLevel(BaseModel):
+    '''
+    Model storing a stock level datapoint
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    lego_set = db.Column(db.Integer, db.ForeignKey('legoset.id'))
+    datetime = db.Column(db.Datetime)
+    stock_level = db.Column(db.Integer)
+
+    def __init__(self, lego_set, stock_level):
+        self.lego_set = lego_set
+        self.stock_level = stock_level
+        self.datetime = dt.utcnow()
--- server/server.py
+++ server/server.py
@@ -3,79 +3,23 @@
 '''
 from datetime import datetime as dt
 from flask import Flask, jsonify, request, g
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate
-# from models import Watch, User, StockLevel
+import controller
 
 app = Flask(__name__)
 app.config.from_envvar('SETTINGS')
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-def connect_db():
-    ''' Connect to Postgres database '''
-    if not hasattr(g, 'db'):
-        g.db = db
-    return g.db
 
 @app.route('/watches', methods=['GET'])
 def get_all_watches():
     ''' Return all watched sets '''
-    return jsonify({'result': 'all watches'})
+    return controller.get_all_watches()
 
 @app.route('/add', methods=['POST'])
 def add_watch():
     ''' Add a watched set to the database, return mongo ID '''
-    return jsonify({'result': 'new_watch'})
-
-
-class Watch(db.Model):
-  '''
-  Model for Watch
-  '''
-  __tablename__ = 'watches'
-  set_id = db.Column(db.Integer, primary_key=True)
-  user = db.Column(db.Integer)
-  added = db.Column(db.Date)
-
-  def __init__(self, set_id, user):
-    self.set_id = set_id
-    self.user = user
-    self.added = dt.utcnow()
-
-  def __repr__(self):
-    return jsonify({'set_id': self.set_id, 'user': self.user, 'added': self.added})
-
-class User(db.Model):
-  '''
-  Model for User
-  '''
-  __tablename__ = 'users'
-  user_id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String)
-
-  def __init__(self, email):
-    self.email = email
-  
-  def __repr__(self):
-    return jsonify({'id': self.user_id, 'email': self.email})
-
-class StockLevel(db.Model):
-  '''
-  Model storing a stock level datapoint
-  '''
-  __tablename__ = 'stocklevels'
-  stocklevel_id = db.Column(db.Integer, primary_key=True)
-  set_id = db.Column(db.Integer)
-  time = db.Column(db.Date)
-  stock_level = db.Column(db.Integer)
-
-  def __init__(self, set_id):
-    self.set_id = set_id
-    self.time = dt.utcnow()
-  
-  def __repr__(self):
-    return jsonify({'id': self.stocklevel_id, 'set_id': self.set_id, 'stock_level': self.stock_level})
+    # TOOD rip stuff off request
+    return controller.add_watch()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
