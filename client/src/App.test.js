c930fce19439247c81e3c0f9e782aee2c8c835cf
Grayson Gilmore <gilmoreg@live.com>
2017-09-29 11:49:44 -0700

    Add raf shim and streamline react tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import { App } from './App';
-
-configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
