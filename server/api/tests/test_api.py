b4d05a2f5005c5e6ff6a43db8b1428d38f532e96
Grayson Gilmore <gilmoreg@live.com>
2017-10-01 21:07:31 -0700

    Update migrations after db volume crash

--- server/api/tests/test_api.py
+++ server/api/tests/test_api.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_testing import TestCase
 import unittest
-from api import db
+from api import db, views
 
 class ApiTestCase(TestCase):
     # Test case class for API
@@ -12,6 +12,7 @@ class ApiTestCase(TestCase):
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://db/legocollector_test'
         app.config['SECRET_KEY'] = 'testing_key'
+        app.config['LIVESERVER_PORT'] = 0
         return app
 
     def setUp(self):
@@ -20,6 +21,11 @@ class ApiTestCase(TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+    def test_get_all_watches(self):
+        response = self.client.get('/watches')
+        print(response)
+
 
 if __name__ == '__main__':
     unittest.main()
