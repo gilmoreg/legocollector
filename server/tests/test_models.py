abc6d4ef1aa6d10a22076e5f75bbf91c4cecc130
Grayson Gilmore <gilmoreg@live.com>
2017-10-07 12:16:21 -0700

    Add more models tests

--- server/tests/test_models.py
+++ server/tests/test_models.py
@@ -4,5 +4,61 @@ from api.models import User, LegoSet, Watch, StockLevel
 
 
 @pytest.mark.usefixtures('db')
-class TestModels:
-    
+class TestUser:
+    ''' Tests for User model '''
+    def test_creation(self):
+        user = User('test@test.com')
+        user.save()
+        retrieved = User.query.filter_by(email='test@test.com').first()
+        assert retrieved == user
+
+
+@pytest.mark.usefixtures('db')
+class TestLegoSet:
+    ''' Tests for LegoSet model '''
+    def test_creation(self):
+        legoset = LegoSet({
+            'id': 12345,
+            'title': 'test',
+            'image': 'test',
+            'url': 'test',
+        })
+        legoset.save()
+        retrieved = LegoSet.query.filter_by(id=12345).first()
+        assert retrieved == legoset
+
+
+@pytest.mark.usefixtures('db')
+class TestWatch:
+    ''' Tests for Watch model '''
+    def test_creation(self):
+        user = User('test@test.com')
+        user.save()
+        legoset = LegoSet({
+            'id': 12345,
+            'title': 'test',
+            'image': 'test',
+            'url': 'test',
+        })
+        legoset.save()
+        watch = Watch(user.id, legoset.id)
+        watch.save()
+        retrieved = Watch.query.filter_by(id=watch.id).first()
+        assert retrieved == watch
+
+
+@pytest.mark.usefixtures('db')
+class TestStockLevel:
+    ''' Tests for StockLevel model '''
+    def test_creation(self):
+        legoset = LegoSet({
+            'id': 12345,
+            'title': 'test',
+            'image': 'test',
+            'url': 'test',
+        })
+        legoset.save()
+        stock_level = StockLevel(legoset.id, 0)
+        stock_level.save()
+        retrieved = StockLevel.query.filter_by(id=stock_level.id).first()
+        assert retrieved == stock_level
