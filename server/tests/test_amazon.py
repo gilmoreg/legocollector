8594678134aaf8d9eed71a5a54c2db577addbd2d
Grayson Gilmore <gilmoreg@live.com>
2017-10-19 16:58:18 -0700

    Add tests for Amazon class

--- server/tests/test_amazon.py
+++ server/tests/test_amazon.py
@@ -1,6 +1,7 @@
 """ Tests for Amazon """
 # noinspection PyCompatibility
 from unittest.mock import Mock, patch
+from urllib.error import HTTPError
 
 import pytest
 
@@ -19,3 +20,17 @@ class TestAmazon:
             response = amazon.search(12345)
             title = response.find('title').get_text()
             assert title == 'Test Set Title'
+
+    def test_error_handler_503(self):
+        """ Test that 503 errors return True """
+        amazon = Amazon()
+        assert amazon.error_handler(err={
+            'exception': HTTPError('/', 503, 'Not available', None, None)
+        }) == True
+
+    def test_error_handler_other(self):
+        """ Test that non-503 errors return False """
+        amazon = Amazon()
+        assert amazon.error_handler(err={
+            'exception': HTTPError('/', 500, None, None, None)
+        }) == False
