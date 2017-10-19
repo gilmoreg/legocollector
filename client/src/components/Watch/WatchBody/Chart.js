8ad0452ef9382e37455cdfc68a16f2fbf76e3b40
Grayson Gilmore <gilmoreg@live.com>
2017-10-19 15:10:33 -0700

    Center Watch components

--- client/src/components/Watch/WatchBody/Chart.js
+++ client/src/components/Watch/WatchBody/Chart.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { LineChart } from 'react-chartjs-2';
+// https://github.com/jerairrest/react-chartjs-2/blob/master/example/src/components/line.js
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+import './Chart.css';
 
 const Chart = (props) => {
-  const createPoints = data =>
-    data.map((point, index) => ({
-      x: index,
-      y: point.stock_level,
-    }));
-
-  const data = [
-    {
-      color: 'steelblue',
-      points: createPoints(props.stock_levels),
+  const data = {
+    labels: props.stock_levels.map(level => moment(level.datetime).format('MM-DD')),
+    datasets: [
+      {
+        label: false,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: props.stock_levels.map(level => level.stock_level),
+      },
+    ],
+  };
+  const options = {
+    legend: {
+      display: false,
     },
-  ];
+  };
 
   return (
-    <LineChart
-      width={600}
-      height={200}
-      data={data}
-      hideXLabel
-      hideYLabel
-      hideXAxis
-      hideYAxis
-    />
+    <div className="Chart">
+      <Line data={data} options={options} />
+    </div>
   );
 };
 
