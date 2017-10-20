80d097f9f00392a78b73e6b396ddd13e19b8735a
Grayson Gilmore <gilmoreg@live.com>
2017-10-20 14:20:59 -0700

    Add footer and style

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -3,7 +3,20 @@ import './Welcome.css';
 
 const Welcome = () => (
   <div className="Welcome">
-    Hi
+    <h2>Welcome to Lego Collector!</h2>
+    <p>
+      If you collect Lego sets for profit, you always want to know when a set is about to run
+      out of stock. Lego Collector keeps you up to date by tracking Amazon&#39;s stock levels
+      for Lego sets you specify.
+    </p>
+    <p>
+      Check the stock levels of all the sets you have your eye on in one convenient place!
+    </p>
+    <p>
+      Soon it will be able to notify you via email when a certain
+      stock falls below a threshold you define so you can snap up one of the last sets before
+      it&#39;s too late!
+    </p>
   </div>
 );
 
