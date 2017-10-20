a0395d55c73b5795dfb7e7ce62ad051c6d484646
Grayson Gilmore <gilmoreg@live.com>
2017-10-20 13:06:26 -0700

    Refactor for expanding Auth components

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Login from '../Login/Login';
+import Login from '../Auth/Login';
 import './Header.css';
 
 const Header = props => (
