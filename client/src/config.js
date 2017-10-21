872250d74affb26a9a7655366124ca9de2abca80
Grayson Gilmore <gilmoreg@live.com>
2017-10-21 14:31:19 -0700

    Fix prod API_URL in config

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export const API_URL = process.env.NODE_ENV === 'production' ?
-  'https://legocollector.herokuapp.com/' :
+  'https://legocollector.herokuapp.com' :
   'http://localhost:5000';
