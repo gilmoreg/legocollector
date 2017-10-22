ebce67ef7c8511b92975e1def4d044ad395d90dc
Grayson Gilmore <gilmoreg@live.com>
2017-10-22 16:07:53 -0700

    Refactor legoset controller

--- server/api/controllers/legoset_controller.py
+++ server/api/controllers/legoset_controller.py
@@ -7,26 +7,29 @@ from api.models import LegoSet, StockLevel
 from api.errors import FlaskError
 from api.controllers.auth_controller import AuthController
 
+print('importing legocontroller')
+
 
 class LegoSetController(object):
     """
-        Controller for legosets
+    Controller for legosets
     """
+    def __init__(self):
+        self.amazon = Amazon()
+
 
     @staticmethod
     def get_legosets():
         """ Get all legoset records """
         return LegoSet.query.all()
 
-    @staticmethod
-    def create_legoset_record(set_id):
+    def create_legoset_record(self, set_id):
         """
-            Add a legoset to the database
-            Presupposes check for pre-existence already done
+        Add a legoset to the database
+        Presupposes check for pre-existence already done
         """
         # Query Amazon API for info about the set
-        amazon = Amazon()
-        response = amazon.search(set_id)
+        response = self.amazon.search(set_id)
         item = response.find('item')
         if item is not None:
             new_legoset = LegoSet({
@@ -44,8 +47,8 @@ class LegoSetController(object):
 
     def add_legoset(self, set_id, token):
         """
-            POST /legoset/add/<id>
-            Fetch data about a legoset from Amazon and add to the database
+        POST /legoset/add/<id>
+        Fetch data about a legoset from Amazon and add to the database
         """
         AuthController.authenticate(token)
         id = str(set_id)
@@ -58,11 +61,11 @@ class LegoSetController(object):
         self.update_stock(new_legoset)
         return new_legoset.to_dict()
 
-    @staticmethod
-    def search(set_id, token):
+
+    def search(self, set_id, token):
         """
-            GET /legoset/search/<id>
-            Queries Amazon for info about this legoset and returns it
+        GET /legoset/search/<id>
+        Queries Amazon for info about this legoset and returns it
         """
         AuthController.authenticate(token)
         # First check in our database
@@ -70,8 +73,7 @@ class LegoSetController(object):
         if set_exists:
             return set_exists.to_dict()
         # If not found, check amazon
-        amazon = Amazon()
-        response = amazon.search(set_id)
+        response = self.amazon.search(set_id)
         item = response.find('item')
         if item is not None:
             return {
@@ -94,14 +96,14 @@ class LegoSetController(object):
             legoset.stock_levels[:] = legoset.stock_levels[-30:]
             legoset.save()
 
-    def update_stock(self, legoset, amazon=Amazon()):
+    def update_stock(self, legoset):
         """
         Add stock level datapoint for specificed set
         @param legoset - LegoSet instance
         @param amazon - Amazon instance
         """
         self.cull_stock(legoset)
-        response = amazon.search(legoset.id)
+        response = self.amazon.search(legoset.id)
         item = response.find('item')
         if item is not None:
             level = item.find('totalnew').get_text()
@@ -116,19 +118,17 @@ class LegoSetController(object):
         @param set_id - int representing set id
         """
         legoset = LegoSet.query.filter_by(id=set_id).first()
-        self.update_stock(legoset, Amazon())
+        self.update_stock(legoset)
 
     def update_stock_levels(self):
         """
         Query Amazon for each Legoset in database,
         add a StockLevel datapoint for each one
-        This function runs on a schedule set up in api/schedule.py
         """
         print('Updating stock levels')
         legosets = self.get_legosets()
-        amazon = Amazon()
         for legoset in legosets:
             try:
-                self.update_stock(legoset, amazon)
+                self.update_stock(legoset)
             except:
                 pass
