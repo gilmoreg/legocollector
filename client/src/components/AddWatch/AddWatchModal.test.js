7e6d32d63d669b82f86f047c09d6e419c19dcf45
Grayson Gilmore <gilmoreg@live.com>
2017-10-26 17:27:50 -0700

    Merge modal and modalcontent

--- client/src/components/AddWatch/AddWatchModal.test.js
+++ client/src/components/AddWatch/AddWatchModal.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import AddWatchModal from './AddWatchModal';
+import { AddWatchModal } from './AddWatchModal';
 
 it('renders without crashing', () => {
   const wrapper = shallow(
