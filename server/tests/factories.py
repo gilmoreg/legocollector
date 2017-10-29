f9d924e54c6124174829657596aae9ab2a30aca3
Grayson Gilmore <gilmoreg@live.com>
2017-10-29 15:39:26 -0700

    Improve documentation

--- server/tests/factories.py
+++ server/tests/factories.py
@@ -4,12 +4,23 @@ from api.models import User, LegoSet
 
 
 def create_user(email):
+    """
+    Add a User to the database and create a valid JWT for it
+    :param email: email address
+    :return: dict of JWT (token), email, User object
+    :rtype: dict
+    """
     user = User(email).save()
     token = create_jwt(user.id)
     return {'token': token, 'email': email, 'user': user}
 
 
 def create_legoset(id):
+    """
+    Create a LegoSet in the database
+    :param id: LEGO set id
+    :return: LegoSet object
+    """
     legoset = LegoSet({
         'id': id,
         'title': 'Test Lego Set',
@@ -20,6 +31,12 @@ def create_legoset(id):
 
 
 def create_watch(legoset, user):
+    """
+    Create a Watch object in the database
+    :param legoset: LegoSet object
+    :param user: User object
+    :return: Watch object
+    """
     user.watches.append(legoset)
     user.save()
     return user
