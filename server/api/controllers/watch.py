19cd544b9350a977bf47ca7a089d42c24146e35b
Grayson Gilmore <gilmoreg@live.com>
2017-10-29 15:22:53 -0700

    Improve documentation

--- server/api/controllers/watch.py
+++ server/api/controllers/watch.py
@@ -6,12 +6,23 @@ from api.errors import FlaskError
 
 
 def get_users_watches(user):
-    """ Return all of a user's Watches """
+    """
+    Return all of a user's Watches
+    :param user: User object
+    :returns: list of Watch objects
+    :rtype: list
+    """
     return list(map(lambda w: w.to_dict(), user.watches))
 
 
 def get_watch(set_id, user):
-    """ Return a specific watch """
+    """
+    Return a specific watch
+    :param set_id: LEGO set id
+    :param user: User object
+    :returns: dict repr of Watch object
+    :rtype: dict
+    """
     for watch in user.watches:
         if watch.id == int(set_id):
             return watch.to_dict()
@@ -21,6 +32,10 @@ def add_watch(legoset, user):
     """
     POST /watch/add
     Add a new watch to the database
+    :param legoset: LegoSet object
+    :param user: User object
+    :returns: dict repr of LegoSet object
+    :rtype: dict
     """
     if legoset in user.watches:
         print('watch already exists for user')
@@ -31,7 +46,13 @@ def add_watch(legoset, user):
 
 
 def delete_watch(set_id, user):
-    """ Cease watching LegoSet set_id for user """
+    """
+    Cease watching LegoSet set_id for user
+    :param set_id: LEGO set id
+    :param user: User object
+    :returns: LEGO set id
+    :rtype: string
+    """
     for watch in user.watches:
         if watch.id == int(set_id):
             user.watches.remove(watch)
