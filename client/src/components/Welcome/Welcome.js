4f670f42a2cd134f63ab7ced30db4c33df6953c6
Grayson Gilmore <gilmoreg@live.com>
2017-11-03 11:45:13 -0700

    Add images to Welcome

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -10,6 +10,16 @@ const Welcome = () => (
       out of stock. Lego Collector keeps you up to date by tracking Amazon&#39;s stock levels
       for Lego sets you specify.
     </p>
+    <div className="images">
+    <img
+        src="http://res.cloudinary.com/dk85nueap/image/upload/c_scale,h_240,w_256/v1509734315/gilmoreg.github.io-legocollector-_iPhone_6_1_tvqhpj.png"
+        alt="example list view"
+      />
+      <img
+        src="http://res.cloudinary.com/dk85nueap/image/upload/c_scale,w_256/v1509733347/gilmoreg.github.io-legocollector-_iPhone_6_zed1iv.png"
+        alt="example stock levels"
+      />
+    </div>
     <p>
       Check the stock levels of all the sets you have your eye on in one convenient place!
     </p>
