773e01d9456be9fb57495105aa3c976f002f681f
Grayson Gilmore <gilmoreg@live.com>
2017-11-08 12:33:46 -0800

    Convert AddWatchButton to styled-components

--- client/src/components/AddWatch/AddWatchButton.js
+++ client/src/components/AddWatch/AddWatchButton.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import './AddWatchButton.css';
+const StyledButton = styled.button`
+  border: 10px solid #FF7F66;
+  border-radius: 50%;
+  background-color: #FF7F66;
+  width: 50px;
+  height: 50px;
+  position: fixed;
+  bottom: 50px;
+  right: 25px;
+  z-index: 0;
+`;
+
+const StyledSpan = styled.span`
+  color: white;
+  font-size: 40px;
+  position: relative;
+  top: -12px;
+  left: -3px;
+  content: "+";
+`;
 
 const AddWatchButton = props => (
-  <button className="AddWatchButton" onClick={props.openModal}>
-    <span>+</span>
-  </button>
+  <StyledButton onClick={props.openModal}>
+    <StyledSpan>+</StyledSpan>
+  </StyledButton>
 );
 
 AddWatchButton.propTypes = {
