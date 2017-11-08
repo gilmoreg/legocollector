61a9bb79115a88dff5198dd6fd527e7af9c0e4a2
Grayson Gilmore <gilmoreg@live.com>
2017-11-08 12:26:25 -0800

    Add more styled-components

--- client/src/components/AddWatch/Examples.js
+++ client/src/components/AddWatch/Examples.js
@@ -10,10 +10,23 @@ const StyledButton = styled.button`
   }
 `;
 
+const StyledUL = styled.ul`
+  font-size: 12px;
+  margin-top: 0.25rem;
+  margin-left: 0;
+  text-align: left;
+  list-style: none;
+`;
+
+const StyledH4 = styled.h4`
+  font-size: 14px;
+  margin-bottom: 0.25rem;
+`;
+
 const Examples = (props) => (
   <div className="Examples">
-    <h4>Examples (click to test out):</h4>
-    <ul>
+    <StyledH4>Examples (click to test out):</StyledH4>
+    <StyledUL>
       <li key="75105">
         <StyledButton data-id={75105} onClick={props.click}>75105 - Star Wars Millenium Falcon</StyledButton>
       </li>
@@ -26,7 +39,7 @@ const Examples = (props) => (
       <li key="10217">
         <StyledButton data-id={10217} onClick={props.click}>10217 - Harry Potter Diagon Alley</StyledButton>
       </li>
-    </ul>
+    </StyledUL>
   </div>
 );
 
