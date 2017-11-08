6e589233fc991f7a1d39eda95fa3cf28e64ee99a
Grayson Gilmore <gilmoreg@live.com>
2017-11-08 12:26:45 -0800

    Remove unused css

--- client/src/components/AddWatch/Examples.js
+++ client/src/components/AddWatch/Examples.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import './Examples.css';
 
 const StyledButton = styled.button`
   text-align: left;
