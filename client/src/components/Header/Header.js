b4ff3d31aa82d3281f81a9200ddcbf17bcd21eb8
Grayson Gilmore <gilmoreg@live.com>
2017-11-08 17:16:17 -0800

    Convert Header to styled-components and update snapshot

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Login from '../Auth/Login';
 import Logout from '../Auth/Logout';
-import './Header.css';
+
+const StyledHeader = styled.header`
+  background: radial-gradient(ellipse at center, rgba(62,69,76,1) 0%,rgba(27,50,73,1));
+  padding: 1rem;
+`;
+
+const StyledH1 = styled.h1`
+  font-family: 'Roboto Slab', 'Courier New', Courier, monospace;
+  color: #FF7F66;
+  margin: 0.25rem;
+`;
 
 const Header = props => (
-  <header className="Header">
-    <h1>Lego Collector</h1>
+  <StyledHeader className="Header">
+    <StyledH1>Lego Collector</StyledH1>
     { props.loggedIn ?
       <Logout /> :
       <Login />
     }
-  </header>
+  </StyledHeader>
 );
 
 Header.defaultProps = {
