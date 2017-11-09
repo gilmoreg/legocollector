ecea75963031679387b1eb7a44abec83c402d023
Grayson Gilmore <gilmoreg@live.com>
2017-11-08 17:24:28 -0800

    Convert WatchFooter to styled-components

--- client/src/components/Watch/WatchFooter/WatchFooter.js
+++ client/src/components/Watch/WatchFooter/WatchFooter.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './WatchFooter.css';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  display: block;
+  background-color: rgba(255,246,229,0.7);
+  border: none;
+  border-radius: 0px 0px 5px 5px;
+  max-width: 760px;
+  width: 100%;
+  margin: 0 auto;
+  z-index: -2;
+`;
 
 const UpArrow = () => (<i className="fa fa-angle-double-up" aria-hidden="true" />);
 const DownArrow = () => (<i className="fa fa-angle-double-down" aria-hidden="true" />);
 
 const WatchFooter = props => (
   <div className="WatchFooter">
-    <button onClick={props.click}>
+    <StyledButton onClick={props.click}>
       { props.collapsed ? <DownArrow /> : <UpArrow /> }
-    </button>
+    </StyledButton>
   </div>
 );
 
