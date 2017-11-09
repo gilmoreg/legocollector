dbd385d970f922cd534e71fd42c6246078200819
Grayson Gilmore <gilmoreg@live.com>
2017-11-08 17:20:56 -0800

    Convert WatchBody to styled-components

--- client/src/components/Watch/WatchBody/WatchBody.js
+++ client/src/components/Watch/WatchBody/WatchBody.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Chart from './Chart';
 
-import './WatchBody.css';
+const StyledDiv = styled.div`
+  background-color: rgba(255,246,229,0.7);
+  max-width: 760px;
+  margin: 0 auto;
+  position: relative;
+  padding-top: 1rem;
+  z-index: -2;
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  top: 1px;
+  right: 1px;
+  font-size: 30px;
+  background-color: rgba(255,246,229,0);
+  color: #3E454C;
+  border: none;
+`;
 
 const WatchBody = props => (
-  <div className="WatchBody">
+  <StyledDiv>
     <Chart stock_levels={props.stock_levels} />
-    <button className="delete" onClick={props.deleteWatchClick}>
+    <StyledButton className="delete" onClick={props.deleteWatchClick}>
       <i className="fa fa-times" aria-hidden="true" />
-    </button>
-  </div>
+    </StyledButton>
+  </StyledDiv>
 );
 
 WatchBody.propTypes = {
